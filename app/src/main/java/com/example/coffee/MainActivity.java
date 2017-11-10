72dd49bd53d14014330340373ccda883987f5596
Cara Ka Lai Yung Yung <cklyyung@uwaterloo.ca>
2017-11-10 10:04:54 -0500

    sliding panel

--- app/src/main/java/com/example/coffee/MainActivity.java
+++ app/src/main/java/com/example/coffee/MainActivity.java
@@ -4,9 +4,12 @@ import android.content.Context;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
+import android.widget.ImageButton;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -16,6 +19,9 @@ public class MainActivity extends AppCompatActivity {
 
     static View.OnClickListener myOnClickListener1;
 
+    ImageButton imageButton;
+    DrawerLayout drawerLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,7 +29,6 @@ public class MainActivity extends AppCompatActivity {
 
         myOnClickListener = new MyOnClickListener(this);
         myOnClickListener1 = new MyOnClickListener1(this);
-
 
         ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
 
@@ -37,6 +42,16 @@ public class MainActivity extends AppCompatActivity {
         ViewPager viewPager1 = findViewById(R.id.view_pager);
         viewPager1.setAdapter(new _PagerAdapter());
         viewPager1.setOffscreenPageLimit(3);
+
+        drawerLayout = findViewById(R.id.drawer);
+
+        imageButton = findViewById(R.id.navigation_button);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawerLayout.openDrawer(Gravity.START);
+            }
+        });
     }
 
     private class MyOnClickListener implements View.OnClickListener {
